f218f88df45556d8cc12604b099f6ca3448141fa
madortill <mador.till@gmail.com>
2025-06-26 09:49:53 +0300

    Update Loader.vue

--- src/components/Loader.vue
+++ src/components/Loader.vue
@@ -1,11 +1,6 @@
 <template>
   <div id="Loader">
-    <img 
-        class="gif" 
-        src="/src/assets/gifs/gif-till.gif" 
-        alt="Loading..." 
-        className="gif-load"
-      />
+    <img class="gif" src="/src/assets/gifs/gif-till.gif" alt="Loading..." className="gif-load" />
     <p class="getting-started">מיד מתחילים...</p>
   </div>
 </template>
@@ -23,17 +18,17 @@ export default {
 <style scoped>
 #Loader {
   display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    height: 55vh; 
-    background-color: #90e0ef;
-    animation: fadeOut 4.5s ease-in-out forwards; 
-    overflow: hidden;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  background-color: #90e0ef;
+  animation: fadeOut 4.5s ease-in-out forwards;
+  overflow: hidden;
 }
 
 .gif-load {
-  width:25rem;
+  width: 25rem;
   height: auto;
   filter: sepia(1) saturate(1000%) hue-rotate(172deg) brightness(0.5);
 }
@@ -46,13 +41,13 @@ export default {
 }
 
 @keyframes fadeOut {
-    0% {
-      opacity: 1; 
-    }
-    100% {
-      opacity: 0; 
-      visibility: hidden; 
-    }
+  0% {
+    opacity: 1;
   }
-  
+
+  100% {
+    opacity: 0;
+    visibility: hidden;
+  }
+}
 </style>
